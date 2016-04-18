47929f0b74877ecb50db526993a9524c0ff7238b
Dalen Gudmunson <dgudmunson@gmail.com>
2016-04-18 15:19:52 -0500

    Adding 'TODO" documentation into my test project to track desired activities.

---
+++ 2as1/2as1.Tests/Documentation/PendingActivity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2as1.Tests.Documentation
+{
+    class PendingActivity
+    {
+        //TODO: Create a welcome page - aligned with the look and feel of a scrapbook
+
+        //TODO: Investigate integrating 'Word Press' into my .NET MVC website - Investigate using 'MiniBlog'
+        
+        //TODO: Investigate the data within MyGTD to see if I can clean up the existing data set
+        
+        //TODO: Create a Bootstrap implementation of 'MyGTD' under 2-as-1.com/mygtd rather than mygtd.2-as-1.com
+        
+         
+    }
+}
